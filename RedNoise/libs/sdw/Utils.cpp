d89f2d8c2b034843d4581cd6326b9bb2ccc48f87
elliot-mb <elliotbuckingham14@gmail.com>
2023-10-02 18:01:55 +0100

    added utils functions to Utils.ccp & h inside sdw

--- RedNoise/libs/sdw/Utils.cpp
+++ RedNoise/libs/sdw/Utils.cpp
@@ -1,8 +1,13 @@
 #include <algorithm>
 #include <sstream>
 #include "Utils.h"
+#include <vector>
+#include <glm/glm.hpp>
 
-std::vector<std::string> split(const std::string &line, char delimiter) {
+using namespace std;
+using namespace glm;
+
+vector<std::string> split(const std::string &line, char delimiter) {
 	auto haystack = line;
 	std::vector<std::string> tokens;
 	size_t pos;
@@ -13,4 +18,31 @@ std::vector<std::string> split(const std::string &line, char delimiter) {
 	// Push the remaining chars onto the vector
 	tokens.push_back(haystack);
 	return tokens;
+}
+
+std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues){
+	if(numberOfValues < 2) throw invalid_argument("interpolateSingleFloats must output at least two values");
+	float delta = to - from;
+	float step = delta / (numberOfValues - 1);
+	vector<float> result = {};
+	for(int i = 0; i < numberOfValues; i++) {
+		result.push_back(from + (i * step));
+	}
+	return result;
+}
+
+std::vector<vec3> interpolateThreeElementValues(vec3 from, vec3 to, int numberOfValues){
+	if(numberOfValues < 2) throw invalid_argument("interpolateThreeElementValues must output at least two values");
+	vector<float> xs = interpolateSingleFloats(from.x, to.x, numberOfValues);
+	vector<float> ys = interpolateSingleFloats(from.y, to.y, numberOfValues);
+	vector<float> zs = interpolateSingleFloats(from.z, to.z, numberOfValues);
+	vector<vec3> result = {};
+	for(int i = 0; i < numberOfValues; i++){
+		result.push_back(vec3(xs[i], ys[i], zs[i]));
+	}
+	return result;
+}
+
+uint32_t pack(uint8_t a, uint8_t r, uint8_t g, uint8_t b) {
+	return (a << 24) + (r << 16) + (g << 8) + b;
 }
--- RedNoise/src/RedNoise.cpp
+++ RedNoise/src/RedNoise.cpp
@@ -11,29 +11,6 @@ using namespace glm;
 #define WIDTH 320
 #define HEIGHT 240
 
-vector<float> interpolateSingleFloats(float from, float to, int numberOfValues){
-	if(numberOfValues < 2) throw invalid_argument("interpolateSingleFloats must output at least two values");
-	float delta = to - from;
-	float step = delta / (numberOfValues - 1);
-	vector<float> result = {};
-	for(int i = 0; i < numberOfValues; i++) {
-		result.push_back(from + (i * step));
-	}
-	return result;
-}
-
-vector<vec3> interpolateThreeElementValues(vec3 from, vec3 to, int numberOfValues){
-	if(numberOfValues < 2) throw invalid_argument("interpolateThreeElementValues must output at least two values");
-	vector<float> xs = interpolateSingleFloats(from.x, to.x, numberOfValues);
-	vector<float> ys = interpolateSingleFloats(from.y, to.y, numberOfValues);
-	vector<float> zs = interpolateSingleFloats(from.z, to.z, numberOfValues);
-	vector<vec3> result = {};
-	for(int i = 0; i < numberOfValues; i++){
-		result.push_back(vec3(xs[i], ys[i], zs[i]));
-	}
-	return result;
-}
-
 void draw(DrawingWindow &window) {
 	window.clearPixels();
 	for (size_t y = 0; y < window.height; y++) {
@@ -53,10 +30,6 @@ void draw(DrawingWindow &window) {
 // 	for(int i=0; i < v.size(); i++) cout << v[i] << " ";
 // 	cout << endl;
 // }
-
-uint32_t pack(uint8_t a, uint8_t r, uint8_t g, uint8_t b) {
-	return (a << 24) + (r << 16) + (g << 8) + b;
-}
 
 void drawGreys(DrawingWindow &window){
 	window.clearPixels();
