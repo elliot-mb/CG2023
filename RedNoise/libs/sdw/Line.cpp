421190681b3a4ad9549fe5c7e49d90a4a6d2cff6
elliot-mb <elliotbuckingham14@gmail.com>
2023-10-09 11:41:26 +0100

    potential vec2 interpolation for late

--- RedNoise/libs/sdw/Line.cpp
+++ RedNoise/libs/sdw/Line.cpp
@@ -3,4 +3,11 @@
 #include "Line.h"
 #include <vector>
 #include <glm/glm.hpp>
+#include "DrawingWindow.h"
 
+using namespace std;
+using namespace glm;
+
+void Line::draw(vec2 posA, vec2 posB, vec3 colour, float weight){
+  
+}
--- RedNoise/libs/sdw/Line.h
+++ RedNoise/libs/sdw/Line.h
@@ -6,12 +6,8 @@
 
 /**
  * Concrete class Line
- * Properties and drawing methods that a line needs to 
- * describe and display itself. 
+ * 'static' class for line methods
 */
-class Line {
-  
-  public: 
-    //render the line with a specified colour and weight
-    const void draw(glm::vec2 posA, glm::vec2 posB, glm::vec3 colour, float weight);
+namespace Line {
+  void draw(glm::vec2 posA, glm::vec2 posB, glm::vec3 colour, float weight);
 };
