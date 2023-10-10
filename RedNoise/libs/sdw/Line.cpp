8c7f5ae55cebe4861a19eca876175f7f82ceb8f2
elliot-mb <elliotbuckingham14@gmail.com>
2023-10-10 13:45:28 +0100

    simple lines

--- RedNoise/libs/sdw/Line.cpp
+++ RedNoise/libs/sdw/Line.cpp
@@ -23,39 +23,11 @@ vector<vec2> Line::pixels(vec2 posA, vec2 posB){
     float steps = Utils::max(abs(delta.x), abs(delta.y));
     if(steps == 0) steps = 1;
     vec2 stepSize = delta / steps;
-    vec2 last = posA;
     vector<vec2> pixels = {};
-    for(float i = 0; i <= steps; i++){
-        vec2 now = posA + (stepSize * i);
-        vec2 lastDelta = round(now) - last;
-        if(lastDelta.x == 0) lastDelta.x = 0.1;
-        if(lastDelta.y == 0) lastDelta.y = 0.1;
-        vec2 signs = abs(lastDelta) / lastDelta;
-        // x
-        for(float j = 0.0; j < abs(lastDelta.x); j++){
-           pixels.push_back(vec2(round(last.x + (j * signs.x)), round(last.y)));
-        }
-        // y
-        for(float j = 0.0; j < abs(lastDelta.y); j++){
-            pixels.push_back(vec2(round(last.x), round(last.y + (j * signs.y))));
-        }
-        last = round(now);
+    for(int i = 0; i <= steps; i++){
+        vec2 now = posA + (stepSize * static_cast<float>(i));
+        pixels.push_back(vec2(round(now.x), round(now.y)));
     }
     pixels.push_back(vec2(round(posB.x), round(posB.y)));
     return pixels;
-}
-
-vector<vec2> Line::points(vec2 posA, vec2 posB){
-    vec2 delta = posB - posA;
-    float steps = Utils::max(abs(delta.x), abs(delta.y));
-    if(steps == 0) steps = 1;
-    vec2 stepSize = delta / steps;
-    vec2 last = posA;
-    vector<vec2> points = {}; //the dots along the line such that there is one for each y level
-    for(float i = 0; i <= steps; i++){
-        vec2 now = posA + (stepSize * i);
-        points.push_back(vec2(round(now.x), round(now.y)));
-    }
-    points.push_back(vec2(round(posB.x), round(posB.y)));
-    return points;
 }
--- RedNoise/libs/sdw/Line.h
+++ RedNoise/libs/sdw/Line.h
@@ -13,6 +13,6 @@
 */
 namespace Line {
     std::vector<glm::vec2> pixels(glm::vec2 posA, glm::vec2 posB); //gives all the pixels we must draw
-    std::vector<glm::vec2> points(glm::vec2 posA, glm::vec2 posB); //just gives points on each step
+    //std::vector<glm::vec2> points(glm::vec2 posA, glm::vec2 posB); //just gives points on each step
     void draw(DrawingWindow& window, glm::vec2 posA, glm::vec2 posB, Colour &colour, float weight);
 };
