e5d9a4c4042cedcf61b3db2bf08e9b85740b1006
elliot-mb <elliotbuckingham14@gmail.com>
2023-10-16 21:31:02 +0100

    remove much junk from the rednoise file

--- RedNoise/src/RedNoise.cpp
+++ RedNoise/src/RedNoise.cpp
@@ -17,21 +17,6 @@ using namespace glm;
 #define WIDTH 320
 #define HEIGHT 240
 
-//GLOBAL!! EVIL!!
-vector<Triangle*> triangles = {
-//        new Triangle(
-//            *new CanvasTriangle(
-//                *new CanvasPoint(160, 10),
-//                *new CanvasPoint(300, 230),
-//                *new CanvasPoint(10,150)),
-//            *new Colour(100, 100, 0),
-//            *new TextureMap("texture.ppm"),
-//            *new CanvasTriangle(
-//                    *new CanvasPoint(195, 5),
-//                    *new CanvasPoint(395, 380),
-//                    *new CanvasPoint(65,330)))
-};
-
 void draw(DrawingWindow &window) {
 	window.clearPixels();
 	for (size_t y = 0; y < window.height; y++) {
@@ -43,50 +28,6 @@ void draw(DrawingWindow &window) {
 			window.setPixelColour(x, y, Utils::pack(255, 127, 127, 127));
 		}
 	}
-}
-
-// template <typename T>
-
-// void showV(vector<T> v) {
-// 	for(int i=0; i < v.size(); i++) cout << v[i] << " ";
-// 	cout << endl;
-// }
-
-void drawGreys(DrawingWindow &window){
-	window.clearPixels();
-	vector<float> greys = Utils::interpolateSingleFloats(255, 0, window.width);
-	vector<uint8_t> greyWholes = {};
-	for(int i = 0; i < greys.size(); i++){
-		greyWholes.push_back(static_cast<uint8_t>(greys[i]));
-	}
-	for(int y = 0; y < window.height; y++){
-		for(int x = 0; x < window.width; x++){
-			uint8_t value = greyWholes[x];
-			uint32_t colour = Utils::pack(255, value, value, value);
-			window.setPixelColour(x, y, colour);
-		}
-	}
-}
-
-void drawColours(DrawingWindow &window){
-	window.clearPixels();
-	vec3 red = vec3(255, 0, 0);
-	vec3 green = vec3(0, 255, 0);
-	vec3 blue = vec3(0, 0, 255);
-	vec3 yellow = vec3(255, 255, 0);
-	vector<vec3> leftCol = Utils::interpolateThreeElementValues(red, yellow, window.height);
-	vector<vec3> rightCol = Utils::interpolateThreeElementValues(blue, green, window.height);
-	for(int y = 0; y < window.height; y++){
-		vector<vec3> row = Utils::interpolateThreeElementValues(leftCol[y], rightCol[y], window.width);
-		for(int x = 0; x < window.width; x++){
-			window.setPixelColour(x, y, Utils::pack(
-				255, 
-				static_cast<uint8_t>(row[x].x),
-				static_cast<uint8_t>(row[x].y),
-				static_cast<uint8_t>(row[x].z)
-			));
-		}
-	} 
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -131,18 +72,7 @@ int main(int argc, char *argv[]) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
         draw(window);
-//		//drawColours(window);
-//        Colour* c = new Colour();
-//		//Line::draw(window, vec2(200.0, 20.0 ), vec2(10.0, 100.0), *c, 1.0);
-//        Line::draw(window, vec2(0.0, 0.0), vec2(160.0, 100.0), *c, 1.0);
-//        Line::draw(window, vec2(320.0, 0.0), vec2(160.0, 100.0), *c, 1.0);
-//        Line::draw(window, vec2(160.0, 0.0), vec2(160, 320), *c, 1.0);
-//        Line::draw(window, vec2(80, 100), vec2(240, 100), *c, 1.0);
 
-        for(int i = 0; i < triangles.size(); i++){
-            //triangles[i]->draw(window);
-            triangles[i]->fill(window);
-        }
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
