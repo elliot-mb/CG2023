abbc6e778be9646aa7fb2b9f24dd3bd764f5049b
elliot-mb <elliotbuckingham14@gmail.com>
2023-11-22 17:03:11 +0000

    fix gouraud

--- RedNoise/src/RedNoise.cpp
+++ RedNoise/src/RedNoise.cpp
@@ -88,7 +88,7 @@ int main(int argc, char *argv[]) {
 
     Scene* s = new Scene({
         new ModelLoader("textured-cornell-box.obj", 0.35, glm::vec3(0, -0.5, 0), ModelLoader::nrm),
-        new ModelLoader("sphere.obj", 0.35, glm::vec3(0, 0, 0), ModelLoader::phg)
+        new ModelLoader("sphere.obj", 0.35, glm::vec3(0, 0, 0), ModelLoader::grd)
     }, {&light});
 
 //
