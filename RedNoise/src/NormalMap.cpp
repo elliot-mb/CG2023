620984864452cfb596fc55b9c90337658a2ddb0f
elliot-mb <elliotbuckingham14@gmail.com>
2023-12-04 18:41:36 +0000

    adding normal maps

---
+++ RedNoise/src/NormalMap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elliot on 04/12/23.
+//
+
+#include "NormalMap.h"
---
+++ RedNoise/src/NormalMap.h
@@ -0,0 +1,14 @@
+//
+// Created by elliot on 04/12/23.
+//
+
+#ifndef REDNOISE_NORMALMAP_H
+#define REDNOISE_NORMALMAP_H
+
+
+class NormalMap {
+
+};
+
+
+#endif //REDNOISE_NORMALMAP_H
